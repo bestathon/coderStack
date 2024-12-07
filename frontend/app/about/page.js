890e5627491ef967562dabf0c3486916599f95ac
bestathon <bestathonteam@gmail.com>
2024-12-07 10:24:33 +0530

    about page

--- frontend/app/about/page.js
+++ frontend/app/about/page.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='mx-auto px-20 py-4'>
+      <p className='text-xl'>
+      Welcome to Coderstacks.com! We are a passionate team dedicated to this platform  which enables programming students to share their learning experience with others,founded in 2024. Our mission is to make the platform where user can showcase their project in the form of text and videos. We believe in making platform engaging and accessible for beginners and advance learners alike, and our goal is to how you solve the problem for your audience. Over the years, we continue to strive for excellence. We’re excited to invite you to browse our services, subscribe to our newsletter. If you have any questions or want to learn more, feel free to reach out to us at coderStack@gmail.com
+      </p>
+    </div>
   )
 }
 
